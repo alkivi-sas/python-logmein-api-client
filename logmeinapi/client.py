5e954fa346db909bcce57ad2f67e8928a50f4d1c
Anthony Martinet <anthony@alkivi.fr>
2017-04-10 17:42:42 +0200

    first commit

--- logmeinapi/client.py
+++ logmeinapi/client.py
@@ -4,9 +4,14 @@
 This module provides a simple python wrapper over the LogMeIn REST API.
 """
 
-from .vendor.requests import Session
+import logging
+import requests
+import json
+
+from requests.exceptions import RequestException
 
 from .config import config
+from .exceptions import APIError
 
 
 #: Default timeout for each request. 180 seconds connect, 180 seconds read.
@@ -15,7 +20,8 @@ TIMEOUT = 180
 
 class Client(object):
     def __init__(self, endpoint=None, company_id=None,
-                 psk=None, timeout=TIMEOUT, config_file=None):
+                 psk=None, timeout=TIMEOUT, config_file=None,
+                 logger=None):
         """
         Creates a new Client. No credential check is done at this point.
         """
@@ -25,21 +31,108 @@ class Client(object):
 
         # load endpoint
         if endpoint is None:
-            self._endpoint = config.get('default', 'endpoint')
-        else:
-            self._endpoint = endpoint
+            endpoint = config.get('default', 'endpoint')
 
         # load keys
         if company_id is None:
             company_id = config.get(endpoint, 'company_id')
         self._company_id = company_id
-
         if psk is None:
             psk = config.get(endpoint, 'psk')
         self._psk = psk
 
-        # use a requests session to reuse HTTPS connections between requests
-        self._session = Session()
+        # variables we use
+        self._authorization = None
+        self._root_url = 'https://secure.logmein.com/public-api'
+
+        # init logger
+        if logger:
+            self.logger = logger
+        else:
+            self.logger = logging.getLogger(__name__)
+
+        self._session = requests.Session()
+        self._session.headers.update({
+            'Accept': 'application/JSON; charset=utf-8',
+            'Authorization': self.get_authorization(),
+        })
 
         # Override default timeout
         self._timeout = timeout
+
+    def get_authorization(self):
+        """Return a json dumps of our authorization."""
+        if not self._authorization:
+            self._authorization = {'companyId': int(self._company_id),
+                                   'psk': self._psk}
+        return json.dumps(self._authorization)
+
+    def call(self, method, path, data=None):
+        """
+        Low level call helper.
+        """
+        # attempt request
+        try:
+            result = self.raw_call(method=method, path=path, data=data)
+        except RequestException as error:
+            raise APIError("Low HTTP request failed error", error)
+
+        status = result.status_code
+
+        # attempt to decode and return the response
+        try:
+            json_result = result.json()
+        except ValueError as error:
+            raise APIError("Failed to decode API response", error)
+
+        # error check
+        if status >= 100 and status < 300:
+            return json_result
+        else:
+            raise APIError(json_result.get('message'), response=result)
+
+    def raw_call(self, method, path, data=None):
+        """
+        Lowest level call helper.
+        """
+        body = ''
+        target = self._root_url + path
+        headers = {}
+
+        self.logger.debug('Going to {0} on url {1}'.format(method, target))
+
+        # include payload
+        if data is not None:
+            headers['Content-type'] = 'application/JSON; charset=utf-8'
+            body = json.dumps(data)
+
+        return self._session.request(method, target, headers=headers,
+                                     data=body, timeout=self._timeout)
+
+    def get(self, _target, **kwargs):
+        """
+        'GET' :py:func:`Client.call` wrapper.
+        """
+        return self.call('GET', _target, kwargs)
+
+    def put(self, _target, **kwargs):
+        """
+        'PUT' :py:func:`Client.call` wrapper.
+        """
+        return self.put('GET', _target, kwargs)
+
+    def post(self, _target, **kwargs):
+        """
+        'POST' :py:func:`Client.call` wrapper.
+        """
+        return self.post('GET', _target, kwargs)
+
+    def delete(self, _target, **kwargs):
+        """
+        'DELETE' :py:func:`Client.call` wrapper.
+        """
+        return self.delete('GET', _target, kwargs)
+
+    def test(self):
+        path = '/v1/authentication'
+        return self.get(path)
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='0.0.1',
 
     description='Lock script to allow only one instance',
     long_description=long_description,
@@ -80,7 +80,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
+    install_requires=['requests'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
