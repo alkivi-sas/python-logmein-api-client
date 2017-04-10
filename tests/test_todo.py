809ae4bfba40790ba4fe2b6ae7a07c0a5d09bfb6
Anthony Martinet <anthony@alkivi.fr>
2017-04-10 17:56:40 +0200

    fix(test)

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -1,2 +1,13 @@
+from logmeinapi import client as logmein
+
+
+def test_connexion():
+    """Test that using a false credentials."""
+    client = logmein.Client(company_id='12345', psk='fake')
+    response = client.test()
+    assert 'success' in response
+    assert not response['success']
+
+
 def test_todo():
     assert 'toto'.capitalize() == 'Toto'
